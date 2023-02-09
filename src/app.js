f1a32b2872ae62aa7b5642ff16c5d0a4d49bcc40
wjohansson <william.johansson1999@outlook.com>
2023-02-09 10:27:43 +0100

    add cube button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
 import { OrbitControls } from 'orbitControls';
-import { GUI } from '../node_modules/dat.gui/build/dat.gui.module.js';
+// import { GUI } from '../node_modules/dat.gui/build/dat.gui.module.js';
 
+const sizeOptions = document.getElementById('size-options');
+const rotationOptions = document.getElementById('rotation-options');
+const lookOptions = document.getElementById('look-options');
+const movementOptions = document.getElementById('movement-options');
+const scale = document.getElementById('scale');
+const xSize = document.getElementById('x-size');
+const ySize = document.getElementById('y-size');
+const zSize = document.getElementById('z-size');
+const xRotation = document.getElementById('x-rotation');
+const yRotation = document.getElementById('y-rotation');
+const zRotation = document.getElementById('z-rotation');
+const texture = document.getElementById('texture')
+const color = document.getElementById('color');
+const metalness = document.getElementById('metalness');
+const opacity = document.getElementById('opacity');
+const autoRotation = document.getElementById('auto-rotate');
+const speed = document.getElementById('speed');
+const bounce = document.getElementById('bounce');
+const wireframe = document.getElementById('wireframe');
+const newCube = document.getElementById('new-cube');
+const addCubeError = document.getElementById('add-cube-error');
 
+scale.value = 1;
+xSize.value = 1;
+ySize.value = 1;
+zSize.value = 1;
+xRotation.value = 0;
+yRotation.value = 0;
+zRotation.value = 0;
+texture.checked = true;
+color.value = '#ffffff';
+metalness.value = 0;
+opacity.value = 1;
+autoRotation.checked = false;
+speed.value = 0.01;
+bounce.checked = true;
+wireframe.checked = false;
 
-const sphereTexture = new THREE.TextureLoader().load('textures/2k_saturn.jpg');
-const ringTexture = new THREE.TextureLoader().load('textures/2k_saturn_ring_alpha.png');
+const boxTexture = new THREE.TextureLoader().load('textures/jupiter.jpg');
+// const sphereTexture = new THREE.TextureLoader().load('textures/2k_saturn.jpg');
+// const ringTexture = new THREE.TextureLoader().load('textures/2k_saturn_ring_alpha.png');
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -32,13 +69,37 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 // sphere.castShadow = true;
 // scene.add(sphere);
 
-const cubeGeometry = new THREE.BoxGeometry(5, 5, 5);
-const cubeMaterial = new THREE.MeshStandardMaterial({ map: sphereTexture});
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cube.position.set(0, 0, 0);
-cube.castShadow = true;
-cube.metalness = 0;
-scene.add(cube);
+var cube;
+var cubeSize = 3;
+var gap = 6;
+var cubeMaterial;
+var cubes = new Array();
+var cubesPosition = [[0, 0], [0, gap], [gap, gap], [gap, 0], [gap, -1 * gap], [0, -1 * gap], [ -1 * gap, -1 * gap], [-1 * gap, 0], [-1 * gap, gap], [-1 * gap, 2 * gap], [0, 2 * gap], [gap, 2 * gap], [2 * gap, 2 * gap], [2 * gap, gap], [2 * gap, 0], [2 * gap, -1 * gap], [2 * gap, -2 * gap], [gap, -2 * gap], [0, -2 * gap], [-1 * gap, -2 * gap], [-2 * gap, -2 * gap], [-2 * gap, -1 * gap], [-2 * gap, 0], [-2 * gap, gap], [-2 * gap, 2 * gap]];
+function addCube()
+{
+    if (cubes.length >= 25)
+    {
+        addCubeError.innerHTML = "Can not add any more cubes";
+        return;
+    }
+
+    const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+    cubeMaterial = new THREE.MeshStandardMaterial({ 
+        transparent: true
+    });
+    cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    // var xPosition = (6 * Math.ceil((cubes.length / 4))) * (cubes.length % 2) * Math.pow(-1, Math.ceil(cubes.length / 2));
+    // var yPosition = (6 * Math.ceil((cubes.length / 4))) * ((cubes.length % 2) - 1) * Math.pow(-1, Math.ceil(cubes.length / 2));
+    var xPosition = cubesPosition[cubes.length][0];
+    var zPosition = cubesPosition[cubes.length][1];
+    cube.position.set(xPosition, 2.5 , zPosition);
+    cube.castShadow = true;
+    scene.add(cube);
+    cubes.push(cube);
+}
+
+addCube();
+
 
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
 const planeMaterial = new THREE.MeshStandardMaterial({
@@ -55,19 +116,19 @@ scene.add(gridHelper);
 const axesHelper = new THREE.AxesHelper(3);
 scene.add(axesHelper);
 
-camera.position.set(10, 10, 10)
-orbit.update(); 
+camera.position.set(20, 20, 20);
+orbit.update();
 
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add( ambientLight );
 
 const directionalLight = new THREE.DirectionalLight(0xc0c0c0, 0.8);
-directionalLight.position.set(30, 50, 0)
+directionalLight.position.set(30, 50, -15);
 directionalLight.castShadow = true;
-directionalLight.shadow.camera.bottom = -15;
-directionalLight.shadow.camera.top = 15;
-directionalLight.shadow.camera.right = 15;
-directionalLight.shadow.camera.left = -15;
+directionalLight.shadow.camera.bottom = -20;
+directionalLight.shadow.camera.top = 20;
+directionalLight.shadow.camera.right = 20;
+directionalLight.shadow.camera.left = -20;
 scene.add( directionalLight );
 
 const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
@@ -76,43 +137,27 @@ scene.add(dLightHelper);
 const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
 scene.add(dLightShadowHelper);
 
-const gui = new GUI();
+// const gui = new GUI();
 
 const options = {
     cubeColor: '#ffffff',
     wireframe: false,
-    speed: 0.01
+    speed: speed.value,
+    bounce: bounce.checked,
+    rotate: autoRotation.checked
 }
 
-gui.addColor(options, 'cubeColor').onChange(function(e){
-    cube.material.color.set(e);
-});
+// gui.addColor(options, 'cubeColor').onChange(function(e){
+//     cube.material.color.set(e);
+// });
 
-gui.add(options, 'wireframe').onChange(function(e){
-    cube.material.wireframe = e;
-});
+// gui.add(options, 'wireframe').onChange(function(e){
+//     cube.material.wireframe = e;
+// });
 
-gui.add(options, 'speed', 0, 0.1)
+// gui.add(options, 'speed', 0, 0.1)
 
 let step = 0;
-
-
-const sizeOptions = document.getElementById('size-options');
-const rotationOptions = document.getElementById('rotation-options');
-const scale = document.getElementById('scale');
-const xSize = document.getElementById('x-size');
-const ySize = document.getElementById('y-size');
-const zSize = document.getElementById('z-size');
-const xRotation = document.getElementById('x-rotation');
-const yRotation = document.getElementById('y-rotation');
-const zRotation = document.getElementById('z-rotation');
-scale.value = 1;
-xSize.value = 1;
-ySize.value = 1;
-zSize.value = 1;
-xRotation.value = 0;
-yRotation.value = 0;
-zRotation.value = 0;
 
 sizeOptions.addEventListener('input', function(){
     cube.scale.set(xSize.value * scale.value, ySize.value * scale.value, zSize.value * scale.value);
@@ -124,24 +169,51 @@ rotationOptions.addEventListener('input', function(){
     cube.rotation.z = zRotation.value * Math.PI * 2;
 });
 
+lookOptions.addEventListener('input', function(){
+    cubeMaterial.map = texture.cecked ? boxTexture : null;
+    cubeMaterial.color.setHex('0x' + color.value.slice(1));
+    cubeMaterial.metalness = metalness.value;
+    cubeMaterial.opacity = opacity.value;
+    cubeMaterial.wireframe = wireframe.checked;
+});
+
+newCube.addEventListener('click', function(){
+    addCube();
+});
+
 window.addEventListener('resize', function() {
     var WIDTH = window.innerWidth,
         HEIGHT = window.innerHeight;
     renderer.setSize(WIDTH, HEIGHT);
     camera.aspect = WIDTH / HEIGHT;
     camera.updateProjectionMatrix();
-  });
+});
+
+function rotateCube(cube) {
+    cube.rotation.y += parseFloat(speed.value);
+    cube.rotation.z += parseFloat(speed.value);
+    cube.rotation.x += parseFloat(speed.value);
+}
+
+function bounceCube(cube) {
+    cube.position.y = 5 * Math.abs(Math.sin(step)) + 2.5;
+}
 
 function animate() {
     requestAnimationFrame( animate );
 
     
+    if (autoRotation.checked)
+    {
+        cubes.forEach(rotateCube);
+    }
 
-    // cube.rotation.x += options.speed;
-    // cube.rotation.y += options.speed;
-
-    step += options.speed;
-    cube.position.y = 5 * Math.abs(Math.sin(step)) + 2.5;
+    if (bounce.checked)
+    {
+        step += parseFloat(speed.value);
+        cubes.forEach(bounceCube)
+    }
+    
 
     renderer.render( scene, camera );
 };
